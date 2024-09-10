dbf316bf072c4ef15b7fdd2fd11465c7351df84a
jon6fingrs <katzjs@gmail.com>
2024-09-10 18:52:22 -0400

    update

--- dockerfile
+++ dockerfile
@@ -15,4 +15,4 @@ VOLUME /mail
 
 COPY run.sh /run.sh
 RUN chmod +x /run.sh
-ENTRYPOINT ["/run.sh"]
+CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+if [ -z ${pipeline_depth} ]; then export pipeline_depth=1024; fi
 
-useradd -m -u ${PUID} -G users,sudo -g users mbsync
-chown mbsync -R /mail
+#useradd -m -u ${PUID} -G users,sudo -g users mbsync
+chown ${PUID} -R /mail
 chmod 774 -R /mail
 sed -i "s/{host_name}/${host_name}/g" /etc/.mbsyncrc
 sed -i "s/{port}/${port}/g" /etc/.mbsyncrc
@@ -12,4 +13,4 @@ sed -i "s/{ssltype}/${ssltype}/g" /etc/.mbsyncrc
 sed -i "s/{sslversions}/${sslversions}/g" /etc/.mbsyncrc
 sed -i "s/{mailbox_name}/${mailbox_name}/g" /etc/.mbsyncrc
 sed -i "s/{pipeline_depth}/${pipeline_depth}/g" /etc/.mbsyncrc
-sudo -u mbsync mbsync -Va -c /etc/.mbsyncrc
+sudo -u \#${PUID} mbsync -Va -c /etc/.mbsyncrc
