5050dc0ac069ad5bb859752e3a8bd674fd6ba213
jon6fingrs <53415122+jon6fingrs@users.noreply.github.com>
2024-09-12 15:33:01 -0400

    Add files via upload

--- dockerfile
+++ dockerfile
@@ -4,7 +4,7 @@ FROM ubuntu:latest
 RUN apt update && apt upgrade -y
 RUN apt install -y isync
 RUN apt install -y openssl
-RUN apt install -y ca-certificates nano sudo
+RUN apt install -y ca-certificates nano sudo cron
 
 
 COPY .mbsyncrc /etc/.mbsyncrc
@@ -12,6 +12,9 @@ RUN mkdir -p /mail
 RUN chmod 777 -R /mail
 VOLUME /mail
 
+COPY root /var/spool/cron/crontabs/root
+RUN chown root:crontab /var/spool/cron/crontabs/root
+RUN chmod 600 /var/spool/cron/crontabs/root
 
 COPY run.sh /run.sh
 RUN chmod +x /run.sh
--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [ -z ${pipeline_depth} ]; then export pipeline_depth=1024; fi
+if [ -z ${ssltype} ]; then export ssltype=IMAPS; fi
+if [ -z ${port} ]; then export port=993; fi
+if [ -z ${sslversions} ]; then export sslversions="[TLSv1] [TLSv1.1] [TLSv1.2] [TLSv1.3]"; fi
 
 #useradd -m -u ${PUID} -G users,sudo -g users mbsync
 chown ${PUID} -R /mail
@@ -14,3 +17,4 @@ sed -i "s/{sslversions}/${sslversions}/g" /etc/.mbsyncrc
 sed -i "s/{mailbox_name}/${mailbox_name}/g" /etc/.mbsyncrc
 sed -i "s/{pipeline_depth}/${pipeline_depth}/g" /etc/.mbsyncrc
 sudo -u \#${PUID} mbsync -Va -c /etc/.mbsyncrc
+if [[ ${cron} = "yes" ]]; then PUID=$PUID cron -f; fi
